28a5178d6880fc3d1d9756a7159f655890adc998
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-02-13 14:01:20 +0900

    [feat] 구구단 정답 화면 표시

--- gugudan/src/App.js
+++ gugudan/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class GuGuDan extends Component {
   constructor(props) {
     super(props);
@@ -12,14 +13,36 @@ class GuGuDan extends Component {
     }
   }
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (parseInt(this.state.value) === this.state.first * this.state.second) {
+      this.setState({
+        first: Math.ceil(Math.random() * 9),
+        second: Math.ceil(Math.random() * 9),
+        value: '', //input에 들어갈 값
+        result: '정답',
+
+      })
+    } else {
+      this.setState({
+        result: '땡',
+        value: '',
+      })
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
   render() {
     return (
       <>
         <div>
           {this.state.first}곱하기{this.state.second}는?
         </div>
-        <form>
-          <input type="number" value={this.state.value} onChange={(e) => this.setState({ value: e.target.value })} />
+        <form onSubmit={this.onSubmit}>
+          <input type="number" value={this.state.value} onChange={this.onChange} />
           <button>입력!</button>
         </form>
         {this.state.result}
